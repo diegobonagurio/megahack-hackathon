03d34c09fcd25c8f3042ba4e3896ada42006f5a9
diegobonagurio <diego.bonagurio@gmail.com>
2020-11-15 22:37:35 -0300

    correcao

--- src/routes/tab.tsx
+++ src/routes/tab.tsx
@@ -1,6 +1,4 @@
 import React from 'react';
-import { Text, View } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
 
 import Payment from '../pages/Payment';
